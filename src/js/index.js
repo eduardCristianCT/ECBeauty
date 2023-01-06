21b427c0120ed57a3bc93addd5ef2117abcae222
eduardCristianCT <95687359+eduardCristianCT@users.noreply.github.com>
2023-01-06 15:46:40 +0200

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -12,3 +12,36 @@ close.addEventListener("click", () => {
     wrapper.toggle("opened");
     wrapper.toggle("close");
 })
+
+var myMap = L.map('map');
+myMap.setView([37.974, 23.72], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+{ attribution:
+'© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(myMap);
+
+var myMap = L.map('map', {
+    center: [37.974, 23.72],
+    zoom: 13,
+    attributionControl: false,
+                zoomControl: false,
+    doubleClickZoom: false,
+    scrollWheelZoom: false,
+    dragging: false,
+    minZoom: 10,
+    maxZoom: 17
+});
+
+myMap.locate({ setView: true, maxZoom: 16 });
+
+var marker = L.marker([37.974, 23.72]).addTo(myMap);
+
+
+var myIcon = L.icon({
+    iconUrl: 'img/marker.png'
+});
+ 
+ 
+var marker = L.marker([37.974, 23.72], {
+    icon: myIcon
+}).addTo(myMap);
